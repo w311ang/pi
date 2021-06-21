cf60b5284ac3f872716f454428ebf3a4c64d1aa9
RZ <30381083+w311ang@users.noreply.github.com>
2021-06-22 03:35:45 +0800

    Update pi.py

--- pi.py
+++ pi.py
@@ -3,11 +3,17 @@ import time
 import random
 import os
 import pickle
+import dateutil.parser
 
 api='https://socialchain.app'
 
+def tostamp(timestr):
+  dt = dateutil.parser.parse(timestr)
+  stamp=int(time.mktime(dt.timetuple()))
+  return stamp
+
 on=os.getenv('on')
-if on=='schedule':
+if False:
   timing=True
 else:
   timing=False
@@ -23,15 +29,15 @@ except FileNotFoundError:
   tokens={}
   token=''
 except KeyError:
-  pass
+  token=''
 session=requests.Session()
+session.headers.update({'authorization':'Bearer '+token})
 if session.get(api+'/api/pi').status_code!=200:
   login=session.post(api+'/api/password_sign_in',data={'phone_number':username,'password':password}).json()
   token=login['credentials']['access_token']
   with open('tokens.txt','wb') as f:
     tokens[username]=token
     pickle.dump(tokens,f)
-session.headers.update({'authorization':'Bearer '+token})
 
 expires=False
 while not expires:
@@ -41,8 +47,19 @@ while not expires:
     time.sleep(3)
   me=session.get(api+'/api/pi').json()
   is_mining=me['mining_status']['is_mining']
+  if is_mining==True:
+    expires_at=me['mining_status']['expires_at']
+    #print(expires_at)
+    expires_at=tostamp(expires_at)
+    now=time.time()
+    diff=expires_at-now
+    #print(diff)
+    if diff>60*60:
+      print('过期时间超过一小时')
+      exit()
+  #print(is_mining)
   #is_mining='false'
-  if is_mining=='false':
+  if is_mining==False:
     expires=True
 
 if timing==True:
